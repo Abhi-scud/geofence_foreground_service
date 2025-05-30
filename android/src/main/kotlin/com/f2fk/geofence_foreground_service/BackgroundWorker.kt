53f9ae7d9bd19bc612381f08b39464dc2c1cfc47
Abhijith <abhi001122abhi@gmail.com>
2025-05-30 15:37:03 +0530

    (fix):hotfix  latitude and longitude issue

--- android/src/main/kotlin/com/f2fk/geofence_foreground_service/BackgroundWorker.kt
+++ android/src/main/kotlin/com/f2fk/geofence_foreground_service/BackgroundWorker.kt
@@ -52,9 +52,9 @@ class BackgroundWorker(
     private val zoneId
         get() = workerParams.inputData.getString(ZONE_ID)!!
     private val latitude
-        get() = workerParams.inputData.getString(latitude)
+        get() = workerParams.inputData.getString(LATITUDE)
         private val longitude
-        get() = workerParams.inputData.getString(longitude)
+        get() = workerParams.inputData.getString(LONGITUDE)
 
     private val isInDebug
         get() = workerParams.inputData.getBoolean(IS_IN_DEBUG_MODE_KEY, false)
